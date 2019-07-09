79e67be04c82356d2a884b8d94722a61e66ea4b5
Or Dicker <or.dicker@gmail.com>
2019-07-09 19:12:58 +0300

    simple test on windows

--- src/main.jl
+++ src/main.jl
@@ -1,1 +1,17 @@
+using Blink
 
+Base.@ccallable function julia_main(ARGS::Vector{String})::Cint
+    w = Window(async=false)
+
+    body!(w, "Hello World")
+    handle(w, "press") do arg
+        println(arg)
+    end
+
+    body!(w, """<button onclick='Blink.msg("", "HELLO")'>go</button>""", async=false);
+
+    while true
+        yield()
+    end
+    return 0;
+end
