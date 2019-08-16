a500793f841bc7f76b2a6a2305e7271021a909cc
kerenKi <kkinberg13@gmail.com>
2019-08-16 17:34:58 +0200

    Added screenshots

--- GUI/src/main.js
+++ GUI/src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.prototype.$black_frames = 'Test'
+
 new Vue({
+  beforeCreate: function() {
+    console.log(this.$black_frames)
+  },
   render: h => h(App),
 }).$mount('#app')
